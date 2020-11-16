41f919613bb6f8d0949a98a5bfe1333168ba8382
EstebanRuiz1 <eduardeste15@gmail.com>
2020-11-16 14:31:50 -0500

    Se añade el for como un loco

---
+++ For como un loco/src/test/For.java
@@ -0,0 +1,9 @@
+package test;
+
+public class For {
+    public static void main(String[] args) {
+        for (int i = 0; i <165651651 ; i++) {
+            System.out.println("For como un loco");
+        }
+    }
+}

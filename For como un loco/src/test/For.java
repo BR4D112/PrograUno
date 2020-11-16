da3a397794a394a163aeddeca3e3c33c0975c8e4
EstebanRuiz1 <eduardeste15@gmail.com>
2020-11-16 14:33:09 -0500

    se añade i como loco

--- For como un loco/src/test/For.java
+++ For como un loco/src/test/For.java
@@ -4,6 +4,7 @@ public class For {
     public static void main(String[] args) {
         for (int i = 0; i <165651651 ; i++) {
             System.out.println("For como un loco");
+            System.out.println(i);
         }
     }
 }

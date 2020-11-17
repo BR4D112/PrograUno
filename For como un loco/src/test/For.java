87e9c78a3f1f7bcfe0528c0b5646d02fd2977524
EstebanRuiz1 <eduardeste15@gmail.com>
2020-11-17 14:44:39 -0500

    se integra un for

--- For como un loco/src/test/For.java
+++ For como un loco/src/test/For.java
@@ -3,7 +3,7 @@ package test;
 public class For {
     public static void main(String[] args) {
         for (int i = 0; i <165651651 ; i++) {
-            System.out.println("For como un loco");
+            System.out.println("Hello world!");
             System.out.println(i);
         }
     }

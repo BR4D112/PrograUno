b630cb49dafcd4e0420482b65b4ad8e295ff8866
EstebanRuiz1 <52499359+EstebanRuiz1@users.noreply.github.com>
2020-11-18 14:41:03 -0500

    Delete Test.java

--- PresentationProject/src/Test.java
+++
@@ -1,9 +0,0 @@
-public class Test {
-    public static void main(String[] args) {
-        System.out.println("hola mundo");
-
-        for (int i = 0; i < 10; i++) {
-            System.out.println("hola");
-        }
-    }
-}

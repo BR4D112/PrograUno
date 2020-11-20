88555e2ce8a2003af9886c5853fd50732ae1bde1
BR4D112 <dalejandro011207@gmail.com>
2020-11-20 12:29:17 -0500

    reorder class Test

--- PicasYFijas/src/Test/Test.java
+++ PicasYFijas/src/Test/Test.java
@@ -13,24 +13,32 @@ import java.util.Scanner;
  * 
  */
 public class Test {
-        Scanner scanner = new Scanner(System.in);
-        int picas = 0, fijas = 0;
-        int longitude = scanner.nextInt();
-        int[] arrayTwo = new int[longitude];
-        int[] arrayOne = new int[longitude];
-    public static void main(String[] args) {
 
-
-
-
-        for (int i = 0; i < arrayOne.length; i++) {
-            if (arrayOne[i] == arrayTwo[i]) fijas++;
-            for (int k = 0; k < arrayOne.length; k++) {
-                int value = arrayOne[i];
-                for (int j = 0; j < arrayTwo.length; j++) if (value == arrayTwo[i]) picas++;
-            }
+    private Scanner scanner;
+    private int picas, fijas;
+    private int longitude;
+    
+    private int[] arrayOne;
+    private int[] arrayTwo;
+    
+    public Test(){
+        scanner = new Scanner(System.in);
+        longitude = scanner.nextInt();
+        picas = 0;
+        fijas = 0;
+        arrayOne = new int[longitude];
+        arrayTwo = new int[longitude];
+        
+    }
+    
+    public void baseNumbers(){
+    
+        for (int i = 0; i < longitude; i++) {
+            System.out.println("Ingrese el numero " + i + " de los numeros a adivinar");
+            arrayOne[i] = scanner.nextInt();
         }
     }
+    
     public void setArrays(){
     System.out.println("Cual va a ser la longitud del juego ");
         for (int i = 0; i < longitude; i++) {
@@ -39,8 +47,10 @@ public class Test {
             arrayTwo[i] = scanner.nextInt();
         }
     }
+    /**
+     * Verifica si son fijas
+     */
     public void verifyFijas(){
-        //Verifica si son fijas
         for (int i = 0; i < longitude; i++) {
             if (arrayTwo[i] == arrayOne[i]) {
                 fijas += 1;
@@ -48,8 +58,10 @@ public class Test {
         }
         System.out.println("Dijitos fijos " + fijas);
     }
+    /**
+     * Verifica si son picas
+    */
     public void verifyPicas(){
-        //Verificar picas
         for (int i = 0; i < longitude; i++) {
             for (int j = 0; j < longitude; j++) {
                 if (arrayTwo[i] == arrayOne[j]&&arrayTwo[i]!=arrayOne[i]) {
@@ -59,13 +71,29 @@ public class Test {
         }
         System.out.println("Picas " + picas);
     }
-    public void baseNumbers(){
+    public void printResult() {
+        System.out.println("deacuerdo a los numeros hay: "+this.fijas+" fijas.");
+        System.out.println("deacuerdo a los numeros hay: "+this.picas+" picas.");
+    }
+    
+    public static void main(String[] args) {
+        
+        Test test = new Test();
+        
+        test.baseNumbers();
+        test.setArrays();
+        test.verifyFijas();
+        test.verifyPicas();
+        
+        test.printResult();
 
-        for (int i = 0; i < longitude; i++) {
-            System.out.println("Ingrese el numero " + i + " de los numeros a adivinar");
-            arrayOne[i] = scanner.nextInt();
+        //for (int i = 0; i < arrayOne.length; i++) {
+        //   if (arrayOne[i] == arrayTwo[i]) fijas++;
+        //    for (int k = 0; k < arrayOne.length; k++) {
+        //        int value = arrayOne[i];
+        //        for (int j = 0; j < arrayTwo.length; j++) if (value == arrayTwo[i]) picas++;
+        //    }
         }
     }
-
 
 }
